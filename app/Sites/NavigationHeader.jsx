71d6e3266697cffb76d49b9ca7b944004c497bb2
Kilian Voss <kilianvoss@proton.me>
2024-02-01 20:43:54 +1100

    fixing header quick attempt

--- app/Sites/NavigationHeader.jsx
+++ app/Sites/NavigationHeader.jsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <div className='flex flex-col md:flex-row bg-sky-100 xl:justify-between xl:items-center xl:p-4 md:p-1'>
       {/* Left Section */}
-      <div className='flex flex-row items-end'>
+      <div className='flex xl:flex-row flex-col  items-end'>
         <img src={Logo.src} alt="renown-roofing-logo" className='mr-2 xl:w-1/4 xl:h-2/4 md:w-fit md:h-fit' />
         <h1 className='font-bold xl:text-5xl md:text-lg'>Renowned Roof Restorations</h1>
       </div>
 
       {/* Right Section */}
-      <ul className='flex items-end mt-4 md:mt-0'> {/* Add margin-top for mobile */}
+      <ul className='flex xl:items-end items-center md:mt-0'> {/* Add margin-top for mobile */}
         <li className='flex sm:m-5'>Home</li>
         <li className='flex sm:m-5'>Services</li>
         <li className='flex sm:m-5'>About</li>
