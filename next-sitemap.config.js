d7a9959a54eae7fe77454495d36896f1cf1db4e0
Kilian Voss <kilianvoss@proton.me>
2024-02-05 16:49:26 +1100

    Installed next sitemap, generated sitemap script in package.json, generated the sitemap xml and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.renownedroofrestorations.com.au',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
