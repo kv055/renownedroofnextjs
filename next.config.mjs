77d25caa0813daba9700a82310570a186cd1541f
Kilian Voss <kilianvoss@proton.me>
2024-02-08 14:45:55 +1100

    CORS CONFIG TO API ROUTE, TESTING IT

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+module.exports = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: '/API/*',
+          
+          headers: [
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin', 
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+          ],
+        },
+      ]
+    }
+  }
+  
