a08ad6ffa538df7435da47ca0ea95600390c9ee4
Kilian Voss <kilianvoss@proton.me>
2024-02-11 01:13:15 +1100

    new meta tag

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <Header /> */}
-      <meta name="ahrefs-site-verification" content="33651fb8e8ae37b5ab3e448023993b4e0613a53f16acacb34057ed0ce7bb8a67"></meta>
-      <meta name="google-site-verification" content="DHni2_22zM6y1yQe3RqcSppu7R7iIJ1_5yup0NYP5OM" />
+      {/* <meta name="ahrefs-site-verification" content="33651fb8e8ae37b5ab3e448023993b4e0613a53f16acacb34057ed0ce7bb8a67"></meta>
+      <meta name="google-site-verification" content="DHni2_22zM6y1yQe3RqcSppu7R7iIJ1_5yup0NYP5OM" /> */}
+      <meta name="google-site-verification" content="ki_nLSSiswIeV1E2ZRm1q9ho-eddZnb65xWbuINHk3g" />
       <body className={LB.className}>{children}</body>
     </html>
   );
